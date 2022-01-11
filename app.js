ece672196992bacaa1345ea5c9ffffa1cb78a0f9
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-11 09:19:41 -0500

    Update markup add position

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ class UI {
   class Store {
     static getBooks() {
       let books;
-      if(localStorage.getItem('books')) {
+      if(localStorage.getItem('books') === null) {
         books = [];
       } else {
         books = JSON.parse(localStorage.getItem('books'));
